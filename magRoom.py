d1d79bcb2b21034ca70073e175bbd1177d542100
Billy-Burns <131682272+Billy-Burns@users.noreply.github.com>
2024-02-20 18:19:43 -0500

    Updates to magroom data pull

---
+++ magRoom.py
@@ -0,0 +1,19 @@
+import requests
+from PyPDF2 import PdfReader
+
+def get_text_from_pdf(pdf_url):
+    text = ""
+    response = requests.get(pdf_url)
+    with open("temp_pdf.pdf", "wb") as pdf_file:
+        pdf_file.write(response.content)
+    with open("temp_pdf.pdf", "rb") as file:
+        reader = PdfReader(file)
+        num_pages = len(reader.pages)
+        for page_num in range(num_pages):
+            page = reader.pages[page_num]
+            text += page.extract_text()
+    return text
+
+pdf_url = "https://dining.wfu.edu/wp-content/uploads/2017/07/mag-room-menu.pdf"
+extracted_text = get_text_from_pdf(pdf_url)
+print(extracted_text)
